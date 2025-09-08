4b444366dd7d66b23de9aa9bbf7e4f09705592ee
LONELONE\mtk45 <maythukhin.uit@gmail.com>
2025-09-08 09:23:48 +0700

    refactor

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region                      = "ap-southeast-2"
-  access_key                  = "4MTWKOXO4PF2FVOH"
-  secret_key                  = "wRG8VwjbfOkCpcfvWHB1hy2JN3j3A9tr5O+"
+  access_key                  = "xxx"
+  secret_key                  = "xxx"
 }
